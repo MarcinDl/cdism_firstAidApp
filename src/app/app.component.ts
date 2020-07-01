f2daa327d093d6ca97c5d4de4450cd19b266bcdb
Marcin Dlubis <marcindl@hotmail.com>
2020-07-01 09:15:49 +0200

    splash screen part I

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
+  routerHidden = true;
+  @ViewChild("splash", { static: false }) splash: ElementRef;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -22,6 +24,11 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      setTimeout(() => {
+        this.routerHidden = false;
+        this.splash.nativeElement.style.display = "none";
+      }, 3000);
     });
   }
 }
